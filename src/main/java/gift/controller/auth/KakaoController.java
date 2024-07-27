8c527e0853af24b8508271209789fc9d5171cec5
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-27 16:41:42 +0900

    feat: BASEURL 상수화 작업 진행
    BASEURL 상수화 진행

--- src/main/java/gift/controller/auth/KakaoController.java
+++ src/main/java/gift/controller/auth/KakaoController.java
@@ -19,7 +19,7 @@ public class KakaoController {
 
     private final KakaoService kakaoService;
     private final KakaoProperties kakaoProperties;
-    private final String oauthBaseUrl = "https://kauth.kakao.com/oauth/authorize?response_type=code&scope=account_email,talk_message";
+    private static final String OAUTH_BASE_URL = "https://kauth.kakao.com/oauth/authorize?response_type=code&scope=account_email,talk_message";
 
     public KakaoController(KakaoService kakaoService, KakaoProperties kakaoProperties) {
         this.kakaoService = kakaoService;
@@ -47,14 +47,14 @@ public class KakaoController {
 
     private HttpHeaders getRedirectHeader(String redirectUri) {
         var headers = new HttpHeaders();
-        String redirectLocation = oauthBaseUrl + "&client_id=" + kakaoProperties.restApiKey() + "&redirect_uri=" + redirectUri;
+        String redirectLocation = OAUTH_BASE_URL + "&client_id=" + kakaoProperties.restApiKey() + "&redirect_uri=" + redirectUri;
         headers.setLocation(URI.create(redirectLocation));
         return headers;
     }
 
     private HttpHeaders getRedirectHeader(String redirectUri, Long memberId) {
         var headers = new HttpHeaders();
-        String redirectLocation = oauthBaseUrl + "&client_id=" + kakaoProperties.restApiKey() + "&redirect_uri=" + redirectUri + "&state=" + memberId;
+        String redirectLocation = OAUTH_BASE_URL + "&client_id=" + kakaoProperties.restApiKey() + "&redirect_uri=" + redirectUri + "&state=" + memberId;
         headers.setLocation(URI.create(redirectLocation));
         return headers;
     }
