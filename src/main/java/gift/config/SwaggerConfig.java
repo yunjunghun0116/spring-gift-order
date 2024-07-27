5b99b454765f35d36865cddb8af964c5a7b69a82
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-28 02:46:56 +0900

    feat: baseOperationResponse 만들어주는 메서드 분리
    메서드 분리 작업

--- src/main/java/gift/config/SwaggerConfig.java
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package gift.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import io.swagger.v3.oas.models.responses.ApiResponse;
@@ -26,47 +27,58 @@ public class SwaggerConfig {
     @Bean
     public OpenApiCustomizer openApiCustomizer() {
         return openApi -> {
-            var excludePaths = Set.of("/api/members/oauth/kakao", "/api/members/login", "/api/members/register", "/api/kakao/get-oauth");
-            openApi.getPaths().forEach((path, pathItem) -> {
-                for (var operation : pathItem.readOperations()) {
-                    var successResponse = new ApiResponse()
-                            .description("성공");
-                    operation.getResponses()
-                            .addApiResponse("200", successResponse);
-                    if (!excludePaths.contains(path)) {
-                        var header = new HeaderParameter()
-                                .name("Authorization")
-                                .required(Boolean.TRUE);
-                        operation.addParametersItem(header);
-
-                        var unauthorizedResponse = new ApiResponse()
-                                .description("잘못된 인증정보");
-                        operation.getResponses()
-                                .addApiResponse("401", unauthorizedResponse);
-                    }
-                }
-                setOperationResponse(pathItem.getPut(), "PUT");
-                setOperationResponse(pathItem.getDelete(), "DELETE");
-            });
+            openApi.getPaths()
+                    .forEach((path, pathItem) -> {
+                        setBaseOperationResponse(path, pathItem);
+                        setOperationResponse(pathItem.getPut(), path, "PUT");
+                        setOperationResponse(pathItem.getPost(), path, "POST");
+                        setOperationResponse(pathItem.getDelete(), path, "DELETE");
+                    });
         };
     }
 
-    private void setOperationResponse(Operation operation, String method) {
+    private void setBaseOperationResponse(String path, PathItem pathItem) {
+        var excludePaths = Set.of("/api/members/oauth/kakao", "/api/members/login", "/api/members/register", "/api/kakao/get-oauth");
+
+        for (var operation : pathItem.readOperations()) {
+            var successResponse = new ApiResponse().description("성공");
+            operation.getResponses()
+                    .addApiResponse("200", successResponse);
+
+            if (!excludePaths.contains(path)) {
+                var header = new HeaderParameter()
+                        .name("Authorization")
+                        .required(Boolean.TRUE);
+                operation.addParametersItem(header);
+
+                var unauthorizedResponse = new ApiResponse().description("잘못된 인증정보");
+                operation.getResponses().addApiResponse("401", unauthorizedResponse);
+            }
+        }
+    }
+
+    private void setOperationResponse(Operation operation, String path, String method) {
         if (operation == null) return;
-        operation.getResponses()
-                .remove("200");
+        if (method.equals("POST") && path.contains("/add")) {
+            var createdResponse = new ApiResponse().description("생성 성공");
+            operation.getResponses()
+                    .addApiResponse("201", createdResponse);
+            operation.getResponses()
+                    .remove("200");
+        }
         if (method.equals("PUT")) {
-            var updatedResponse = new ApiResponse()
-                    .description("업데이트 성공");
+            var updatedResponse = new ApiResponse().description("업데이트 성공");
             operation.getResponses()
                     .addApiResponse("204", updatedResponse);
-            return;
+            operation.getResponses()
+                    .remove("200");
         }
         if (method.equals("DELETE")) {
-            var updatedResponse = new ApiResponse()
-                    .description("삭제 성공");
+            var updatedResponse = new ApiResponse().description("삭제 성공");
             operation.getResponses()
                     .addApiResponse("204", updatedResponse);
+            operation.getResponses()
+                    .remove("200");
         }
     }
 }
